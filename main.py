d46e54ffa2395efa97a619a8045f8874eb3204b5
Adrienne Wright <ad@rienne.de>
2023-01-28 16:27:12 +0100

    main scaffold

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+1. Check for new mentions.
+2. Write mentions to json.
+3. Send unprocessed tweets to Prompt Generator.
+4. Send first unprocessed tweet to Dreamer.
+5. Send generated images to Style Transferer.
+6. Tweet images.
+7. Mark tweet as processed.
+Repeat
+"""
