e24a881a92e2665feb41f74261e1e4bd8cc39226
Adrienne Wright <ad@rienne.de>
2023-01-29 05:38:45 +0100

    add :

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import os
 def get_next_tweet(df: pd.DataFrame) -> pd.DataFrame:
     unprocessed_tweets = df.where((df['processed'] == False) & (~df['media_key'].isnull()))
     unprocessed_tweets.sort_values(by=['created_at'], inplace=True)
-    if unprocessed_tweets.empty()
+    if unprocessed_tweets.empty():
         return False
     else:
         return unprocessed_tweets.head(1)
