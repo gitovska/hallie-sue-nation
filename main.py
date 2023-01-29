f92e3d36bb1c11134f0996fa292f78153146d8e5
Adrienne Wright <ad@rienne.de>
2023-01-29 01:28:17 +0100

    only return tweets with media and add console printing

--- main.py
+++ main.py
@@ -8,7 +8,6 @@
 7. Mark tweet as processed.
 Repeat
 """
-import pandas
 
 from TwitterBot import TwitterBot
 from Preprocessor import Preprocessor
@@ -17,8 +16,8 @@ import pandas as pd
 
 
 def get_next_tweet(df: pd.DataFrame) -> pd.DataFrame:
-    unprocessed_tweets = df.where(df['processed'] == False)
-    unprocessed_tweets.sort_values(by=['created_at'], inplace=True, ascending=False)
+    unprocessed_tweets = df.where((df['processed'] == False) & (~df['media_key'].isnull()))
+    unprocessed_tweets.sort_values(by=['created_at'], inplace=True)
     return unprocessed_tweets.head(1)
 
 def get_prompts(tweet: str) -> list[str]:
@@ -28,15 +27,26 @@ def get_prompts(tweet: str) -> list[str]:
 
 if __name__ == "__main__":
 
+    # get mentions
     bot = TwitterBot()
-    # bot.mentions("HallieSueNation")
+    bot_name = "HallieSueNation"
+    print(f"Getting mentions for {bot_name}")
+    bot.mentions(botname)
+
+    # get next unprocessed tweet
     df = pd.read_json('data/mentions.json')
     next_tweet = get_next_tweet(df)
-    prompts = get_prompts(next_tweet['text'].values[0])
-    url = next_tweet['url'].values[0]
+    next_tweet_text = next_tweet['text'].values[0]
+    next_tweet_prompts = get_prompts(next_tweet_text)
+    next_tweet_url = next_tweet['url'].values[0]
     next_tweet_id = (int(next_tweet['id'].values[0]))
 
-    #dreamer = Dreamer()
-    #dreamer.dream(prompts, tweet_id=next_tweet_id, image_url=url)
+    # dream
+    print(f"Dreaming with tweet: '{next_tweet_text}' and the following prompts:")
+    for prompt in next_tweet_prompts:
+        print(f"\t{prompt}")
+    dreamer = Dreamer()
+    dreamer.dream(prompts, tweet_id=next_tweet_id, image_url=next_tweet_url)
 
-    #bot.reply(next_tweet_id, image)
+    # tweet back with dream sequence
+    # bot.reply(next_tweet_id, image)
